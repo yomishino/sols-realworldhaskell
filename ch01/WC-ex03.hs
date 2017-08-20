43643b9489d6a75615c13f82650b18074af3b3b4
yomishino <yomishino@users.noreply.github.com>
2017-08-20 16:45:01 +1000

    Add solutions to ch01 exercises

---
+++ ch01/WC-ex03.hs
@@ -0,0 +1,11 @@
+{-
+    File: WC-ex03.hs
+    Author: yomishino
+
+    Chapter 1, Exercise 4
+
+    Count the number of characters in a file.
+-}
+
+main = interact count
+    where count input = show (length input) ++ "\n"
---
+++ ch01/WC-ex04.hs
@@ -0,0 +1,12 @@
+{-
+    File: WC-ex04.hs   
+     Author: yomishino
+
+    Chapter 1, Exercise 4
+    
+    Count the number of characters in a file.
+-}
+
+main = interact count
+    where count input = show (length input) ++ "\n"
+
